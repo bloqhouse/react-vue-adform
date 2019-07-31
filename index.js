a27ac56481176a92130303afc5bef296e23fcb6b
Jesus <jsfuentes@alu.ucam.edu>
2019-07-31 12:25:39 +0200

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const adform = require('./src/adform');
 
 module.exports = {
-	adform: {
-		initialize: function initialize(params) {
-			adform(params);
-		}
-	}
+  adform: {
+    initialize: function initialize(params) {
+      adform(params);
+    }
+  }
 };
--- src/adform.js
+++ src/adform.js
@@ -1,12 +1,12 @@
-module.exports = function(params) {
-	(function(w, d) {
-		w._adftrack = Array.isArray(w._adftrack) ? w._adftrack : (w._adftrack ? [w._adftrack] : []);
-		w._adftrack.push({ ...params });
-		const s = d.createElement('script');
-		s.type = 'text/javascript';
-		s.async = true;
-		s.src = 'https://track.adform.net/serving/scripts/trackpoint/async/';
-		const x = d.getElementsByTagName('script')[0];
-		x.parentNode.insertBefore(s, x);
-	})(window, document);
+module.exports = function (params) {
+  (function (w, d) {
+    w._adftrack = Array.isArray(w._adftrack) ? w._adftrack : (w._adftrack ? [w._adftrack] : []);
+    w._adftrack.push({ ...params });
+    const s = d.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://track.adform.net/serving/scripts/trackpoint/async/';
+    const x = d.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  })(window, document);
 };
